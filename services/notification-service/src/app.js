ae1cd99fa84fb461673c0e0194bddbbb5570b918
Dudariis <dudasilva51792@gmail.com>
2026-08-06 10:36:39 -0300

    Cria model de Notification e repositório em memória no notification-service

--- services/notification-service/src/app.js
+++ services/notification-service/src/app.js
@@ -1,12 +1,17 @@
-import express, { json } from "express";
-import cors from "cors";
-import notificationRoutes from "./routes/notificationRoutes.js";
+import express from "express"; // Importa o Express que é a ferramenta que cria servidores web, a base.
+import cors from "cors"; // Importa o Cors que é a ferramenta que deixa os outros sistemas acessarem.
 
-const app = express ();
+const app = express (); // const cria a aplicação
 
-app.use (cors ());
-app.use (express.json ());
-app.use ("/notifications", notificationRoutes);
+app.use (cors ()); // responsável por ativar o cors, sem ele, os outros sistemas não respondem.
+app.use (express.json ()); // Responsável por ativar a leitura do json.
+
+// É uma rota de testes, no caso, se vc acessar o site http://localhost:3004, ele responderá com um OK.
+app.get ("/", (req, res) => {
+    res.json ({ status: "ok", servico: "notification-service" });
+});
+
+export default app; // Ele é responsável por entregar o app para os outros arquivos e o server.js importa ele.
 
 
-export default app;
+// o app.js disponibiliza o app para o server.js usar.
--- services/notification-service/src/server.js
+++ services/notification-service/src/server.js
@@ -1,8 +1,15 @@
-import app from "./app.js";
+// server.js é responsável pela inicialização do serviço
 
-const PORT = 3004;
+import app from "./app.js"; // import significa importar (buscar) o arquivo app.js
 
+const PORT = 3004; // Const (constante, um valor que não muda), Port (porta) que é o número onde o serviço vai chamar
 
+// Responável por ligar o servidor na porta (PORT) 3004 e o console.log mostrar o resultado
 app.listen (PORT, () => {
     console.log (`Notification Service rodando na porta ${PORT}`);
-})
+});
+
+
+
+// Ou seja, o server é responsável por buscar a aplicação (app), conectar na porta (3004),
+// e fica esperando a mensagem mostrar se está funcionando ou não no terminal.
